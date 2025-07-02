af41f6fce555f7210d0abd1bd5a7cc486e2039f4
Darren Choong <darren@easyprintsg.com>
2025-07-02 12:18:38 +0800

    fix: restructure for Vercel - move build config to root with frontend source directory

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: 'frontend',
+  publicDir: resolve('./frontend/public'),
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: '../dist',
+    sourcemap: true,
+    emptyOutDir: true
+  }
+}) 
